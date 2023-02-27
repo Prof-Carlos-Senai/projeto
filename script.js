ee038bca82cf955bbf89744339d7dbb70d734874
Carlos <silva@edu.sc.senai.br>
2023-02-27 16:06:20 -0300

    Acrescentado Javascript

---
+++ script.js
@@ -0,0 +1,9 @@
+function gravar(){
+    let nome = document.getElementById('nome').value
+    console.log(nome)
+
+    let res = document.getElementById('res')
+    res.style.fontSize = '20px'
+    res.style.fontFamily = 'Tahoma'
+    res.style.fontWeight = 'bold'
+}
