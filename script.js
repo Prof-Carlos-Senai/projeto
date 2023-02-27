3793ab83fa4837971f94c012fdc957e25a2bd086
Carlos <silva@edu.sc.senai.br>
2023-02-27 16:46:37 -0300

    Acrescentado email

--- script.js
+++ script.js
@@ -2,9 +2,18 @@ function gravar(){
     let nome = document.getElementById('nome').value
     console.log(nome)
 
+    let email = document.getElementById('email').value
+    console.log(email)
+
     let res = document.getElementById('res')
     res.style.fontSize = '20px'
     res.style.fontFamily = 'Tahoma'
     res.style.fontWeight = 'bold'
     res.innerHTML = nome
+
+    let res2 = document.getElementById('res2')
+    res2.style.fontSize = '20px'
+    res2.style.fontFamily = 'Tahoma'
+    res2.style.fontWeight = 'bold'
+    res2.innerHTML = email
 }
